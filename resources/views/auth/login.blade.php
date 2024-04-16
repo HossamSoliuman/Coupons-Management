b091a49809e3d1b6b2913ce85768600fab3eac37
Hossam <hossamsoliuman@gmail.com>
2024-04-16 08:58:26 +0200

    translating login to arabic

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -157,15 +157,15 @@
         </div>
         <form method="POST" action="{{ route('login') }}">
             @csrf
-            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
-            <input id="username" class="form-control" id="email" type="email" @error('email') is-invalid @enderror name="email"
-                value="{{ old('email') }}" required autocomplete="email" autofocus>
+            <label for="email" class="col-md-4 col-form-label text-md-end">البريد الالكتروني</label>
+            <input id="username" class="form-control" id="email" type="email" @error('email') is-invalid @enderror
+                name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
-            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+            <label for="password" class="col-md-4 col-form-label text-md-end">الرقم السري</label>
             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
                 name="password" required autocomplete="current-password">
 
